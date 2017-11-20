86c4b93206e5d9cc277fac8f709c6131fd281af0
Bryan Li <bryanlimy@gmail.com>
2017-11-20 00:02:54 -0500

    added TA

--- website/app/server/users.js
+++ website/app/server/users.js
@@ -21,3 +21,25 @@ Accounts.onCreateUser(function(options, user) {
 	// Returns the user object
 	return user
 })
+
+
+Meteor.publish('userData', function () {
+	var user = Meteor.user()
+	if (user) {
+		if (user.profile.accountType === 'instructor') {
+			return Meteor.users.find({}, {
+				fields: {
+					'profile': 1
+				}
+			})
+		} else {
+			return Meteor.users.find({}, {
+				fields: {
+					'profile.first_name': 1,
+					'profile.last_name': 1,
+					'profile.picture': 1
+				}
+			})
+		}
+	}
+})
