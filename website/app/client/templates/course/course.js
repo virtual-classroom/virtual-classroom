0e84addbe8d15b657a2a04aaffb3589e3d0223b1
Bryan Li <bryanlimy@gmail.com>
2017-11-20 00:02:54 -0500

    added TA

--- website/app/client/templates/course/course.js
+++ website/app/client/templates/course/course.js
@@ -6,42 +6,58 @@ Template.Course.events({
 		$('#create-lecture-modal').modal('open')
 	},
 	'click #cancel-create-lecture': function() {
-		//Reset form
-		$('#lectureName').val('')
-		$('#lectureName').removeClass("invalid")
+		resetCreateLectureForm()
 		$('#create-lecture-modal').modal('close')
 	},
 	'submit #create-lecture-form': function(event) {
-		event.preventDefault();
-		target = event.target
-		var title = $('#lectureName').val()
-		var user = Meteor.user()
-		var course = Courses.findOne({code: Session.get('courseCode')})
-		if (title == "") {
-			$("#lectureName").removeClass("validate")
-			$("#lectureName").removeClass("valid")
-			$("#lectureName").addClass("invalid")
-			$("#lectureName-label").attr("data-error", "Please give this lecture a title")
-			Session.set("validLectureSection", false)
-		} else if (Lectures.findOne({'lecture':title})) {
-			$("#lectureName").removeClass("validate")
-			$("#lectureName").removeClass("valid")
-			$("#lectureName").addClass("invalid")
-			$("#lectureName-label").attr("data-error", "This lecture already exists")
-			Session.set("validLectureSection", false)
-		} else if (course && course.instructors.indexOf(user._id) >= 0) {
-			title = title.replace(/\?/g,'')
-			Meteor.call('addLecture', title, course.code, function(error, result) {
-				if (error) {
-					console.log(error)
-					Materialize.toast('Error: ' + error.message, 8000)
-				} else {
-					$('#lectureName').val('')
-					$('#lectureName').removeClass("invalid")
-					$('#create-lecture-modal').modal('close')
-					Materialize.toast('Lecture ' + title + ' has been created!', 4000)
+		event.preventDefault()
+		if (userIsCourseInstructor()) {
+			var title = $('#lectureName').val()
+			var user = Meteor.user()
+			var course = Courses.findOne({code: Session.get('courseCode')})
+			if (title == "") {
+				$("#lectureName").removeClass("validate")
+				$("#lectureName").removeClass("valid")
+				$("#lectureName").addClass("invalid")
+				$("#lectureName-label").attr("data-error", "Please give this lecture a title")
+				Session.set("validLectureSection", false)
+			} else if (Lectures.findOne({'lecture':title})) {
+				$("#lectureName").removeClass("validate")
+				$("#lectureName").removeClass("valid")
+				$("#lectureName").addClass("invalid")
+				$("#lectureName-label").attr("data-error", "This lecture already exists")
+				Session.set("validLectureSection", false)
+			} else {
+				title = title.replace(/\?/g,'')
+				Meteor.call('addLecture', title, course.code, function(error, result) {
+					if (error) {
+						console.log(error)
+						Materialize.toast('Error: ' + error.message, 8000)
+					} else {
+						resetCreateLectureForm()
+						$('#create-lecture-modal').modal('close')
+						Materialize.toast('Lecture ' + title + ' has been created!', 4000)
+					}
+				})
+			}			
+		}
+	},
+	'submit #course-settings-form': function(event) {
+		event.preventDefault()
+		if (userIsCourseOwner()) {
+			var instructors = $('#selectCourseInstructors').val()
+			if (!instructors) instructors = []
+			settings = {}
+			settings.instructors = instructors
+			Meteor.call("updateCourseSettings", Session.get('courseCode'), settings, 
+				function(error, result) {
+					if (error) {
+						console.log(error)
+					} else {
+						Materialize.toast('Course settings updated', 4000)
+					}
 				}
-			})
+			)
 		}
 	}
 });
@@ -56,14 +72,42 @@ Template.Course.helpers({
 		if (lectures.count() > 0) return lectures
 	},
 	course: function() {
-		//return course
 		var course = Courses.findOne({
 			'code': Session.get('courseCode'),
 			'status': 'active'
 		})
 		if (course) return course
+	},
+	userIsCourseInstructorSelect: function(userId) {
+		if (userIsCourseInstructor(userId)) return 'selected'
+	},
+	userIsCourseOwnerSelect: function(userId) {
+		if (userIsCourseOwner(userId)) return 'disabled'
 	}
 });
+
+function userIsCourseInstructor(userId) {
+	var course = Courses.findOne({code: Session.get('courseCode')})
+	if (course) {
+		if (userId) return course.instructors.indexOf(userId) >= 0
+		else return course.instructors.indexOf(Meteor.userId()) >= 0
+	}
+}
+
+function userIsCourseOwner(userId) {
+	var course = Courses.findOne({code: Session.get('courseCode')})
+	if (course) {
+		if (userId) return course.ownerId == userId
+		else return course.ownerId == Meteor.userId()
+	}
+}
+
+function resetCreateLectureForm() {
+	$('#lectureName').val('')
+	$('#lectureName').addClass('valid')
+	$('#lectureName').removeClass("invalid")
+	$('#lectureName-label').removeClass('active')
+}
 
 /*****************************************************************************/
 /* Course: Lifecycle Hooks */
@@ -72,7 +116,12 @@ Template.Course.onCreated(function () {
 });
 
 Template.Course.onRendered(function () {
-	$('#create-lecture-modal').modal()
+	if (userIsCourseInstructor()) {
+		$('#create-lecture-modal').modal()
+	}
+	Meteor.setTimeout(function() {
+		$('#selectCourseInstructors').material_select()
+	}, 50)
 	Session.set("validLectureSection", false)
 	var courseCode = Router.current().params.code
 	if (courseCode) Session.set('courseCode', courseCode.toUpperCase())
--- website/app/server/publish.js
+++ website/app/server/publish.js
@@ -1,23 +1,23 @@
 // Meteor.publish('usersEmail', function() {
 // 	return Meteor.users.find({}, {fields: {'emails': 1}});
 // });
-Meteor.publish('userData', function () {
-	var user = Meteor.user()
-	if (user) {
-		if (user.profile.accountType === 'instructor') {
-			return Meteor.users.find({}, {
-				fields: {
-					'profile': 1
-				}
-			})
-		} else {
-			return Meteor.users.find({}, {
-				fields: {
-					'profile.first_name': 1,
-					'profile.last_name': 1,
-					'profile.picture': 1
-				}
-			})
-		}
-	}
-})
+// Meteor.publish('userData', function () {
+// 	var user = Meteor.user()
+// 	if (user) {
+// 		if (user.profile.accountType === 'instructor') {
+// 			return Meteor.users.find({}, {
+// 				fields: {
+// 					'profile': 1
+// 				}
+// 			})
+// 		} else {
+// 			return Meteor.users.find({}, {
+// 				fields: {
+// 					'profile.first_name': 1,
+// 					'profile.last_name': 1,
+// 					'profile.picture': 1
+// 				}
+// 			})
+// 		}
+// 	}
+// })
